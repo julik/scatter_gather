6d2be47f74832547583d3a9d5980f91703f37e33
Julik Tarkhanov <me@julik.nl>
2025-10-30 13:02:21 +0100

    Continue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,10 @@ require_relative "../test/dummy/config/environment"
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 require "rails/test_help"
 
+# Have Rails prepare the test database from schema.rb (or migrations if needed)
+ActiveRecord::Schema.verbose = false
+ActiveRecord::Migration.maintain_test_schema!
+
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
